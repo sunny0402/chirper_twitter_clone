8de67fdea7560f3373fc45b3c591dd3e7a224ff7
sunny0402 <sunnycodes0402@gmail.com>
2022-06-11 15:39:07 -0400

    Handle initial data load.

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,7 +1,7 @@
 import { getInitialData } from "../utils/api";
-import { receiveUsers } from "./actions/users";
-import { receiveTweets } from "./actions/tweets";
-import { setAuthedUser } from "./actions/authedUser";
+import { receiveUsers } from "./users";
+import { receiveTweets } from "./tweets";
+import { setAuthedUser } from "./authedUser";
 
 const AUTHED_ID = "tylermcginnis";
 
