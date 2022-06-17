32d07e285ca1abd4e1091aabc57e4eab92894996
sunny0402 <sunnycodes0402@gmail.com>
2022-06-17 12:23:42 -0400

    Toggle like tweet.

--- src/reducers/tweets.js
+++ src/reducers/tweets.js
@@ -1,4 +1,12 @@
-import { RECEIVE_TWEETS } from "../actions/tweets";
+import { RECEIVE_TWEETS, TOGGLE_TWEET } from "../actions/tweets";
+
+/* 
+When liking a tweet (or unliking a tweet), the state for that specific tweet needs 
+to change - either the tweet's like property (which, if you remember, is an array and 
+will contain the names of the users that have liked the tweet) will need to change to 
+include the user that clicked it (if they're liking the tweet) or the user's name will 
+need to be removed from the array (if they're unliking the tweet).
+**/
 
 export default function tweets(state = {}, action) {
   switch (action.type) {
@@ -6,6 +14,19 @@ export default function tweets(state = {}, action) {
       return {
         ...state,
         ...action.tweets,
+      };
+    case TOGGLE_TWEET:
+      return {
+        ...state,
+        [action.id]: {
+          ...state[action.id],
+          likes:
+            action.hasLiked === true
+              ? state[action.id].likes.filter(
+                  (uid) => uid !== action.authedUser
+                )
+              : state[action.id].likes.concat([action.authedUser]),
+        },
       };
     default:
       return state;
